0b23041c407fea6c390f0be8bc74006e690b42b8
elite <elitemythyt@gmail.com>
2020-12-29 09:49:45 +0530

    Make it fill the remaining space after the code with comments or spaces.

--- helpers.js
+++ helpers.js
@@ -1,23 +1,23 @@
 function getCharsUntilNextSpace(text) {
-  let count = 0;
-  for (const char of text) {
-    if (char !== " " && char !== "\n") {
-      count++;
-    } else {
-      return count;
-    }
-  }
+	let count = 0;
+	for (const char of text) {
+		if (char !== " " && char !== "\n") {
+			count++;
+		} else {
+			return count;
+		}
+	}
 }
 
 function getCharsUntilNextNonSpace(text) {
-  let count = 0;
-  for (const char of text) {
-    if (char === " ") {
-      count++;
-    } else {
-      return count;
-    }
-  }
+	let count = 0;
+	for (const char of text) {
+		if (char === " ") {
+			count++;
+		} else {
+			return count;
+		}
+	}
 }
 
 // if, else, etc. also don't necessarily need a space, but only when a block statement follows
@@ -26,27 +26,36 @@ function getCharsUntilNextNonSpace(text) {
 const noSpaceKeywords = ["for", "switch", "break", "while", "this"];
 
 function needsSpaceAfter(token, nextToken) {
-  // there has to be a better way... and now the if chain begins!
-  if (
-    (token.type === "Keyword" && !noSpaceKeywords.includes(token.value)) ||
-    (token.value === "else" && nextToken.value === "if") ||
-    (token.type === "Identifier" && nextToken.value === "of") ||
-    (token.value === "of" && nextToken.type === "Identifier") ||
-    (token.type === "Identifier" && nextToken.value === "in") ||
-    (token.value === "in" && nextToken.type === "Identifier") ||
-    (token.type === "Identifier" && nextToken.value === "instanceof") ||
-    (token.value === "instanceof" && nextToken.type === "Identifier") ||
-    (token.value === "await" && nextToken.type === "Identifier") ||
-    (token.value === "async" && nextToken.value === "function")
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+	// there has to be a better way... and now the if chain begins!
+	if (
+		(token.type === "Keyword" && !noSpaceKeywords.includes(token.value)) ||
+		(token.value === "else" && nextToken.value === "if") ||
+		(token.type === "Identifier" && nextToken.value === "of") ||
+		(token.value === "of" && nextToken.type === "Identifier") ||
+		(token.type === "Identifier" && nextToken.value === "in") ||
+		(token.value === "in" && nextToken.type === "Identifier") ||
+		(token.type === "Identifier" && nextToken.value === "instanceof") ||
+		(token.value === "instanceof" && nextToken.type === "Identifier") ||
+		(token.value === "await" && nextToken.type === "Identifier") ||
+		(token.value === "async" && nextToken.value === "function")
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getAsciiArtInfo(asciiArt) {
+	let width = asciiArt.split("\n")[0].length;
+	let height = asciiArt.split("\n").length;
+	let totalNumberOfChars = width * height;
+	let totalNumberOfNonSpace = (asciiArt.match(/#/g) || []).length;
+	return { width, height, totalNumberOfChars, totalNumberOfNonSpace };
 }
 
 module.exports = {
-  getCharsUntilNextSpace,
-  getCharsUntilNextNonSpace,
-  needsSpaceAfter,
+	getCharsUntilNextSpace,
+	getCharsUntilNextNonSpace,
+	needsSpaceAfter,
+	getAsciiArtInfo,
 };
--- index.js
+++ index.js
@@ -1,87 +1,106 @@
-"use strict";
-
 const fs = require("fs");
 const { tokenize } = require("esprima");
 const { minify } = require("terser");
 const { ArgumentParser } = require("argparse");
 
 const {
-  getCharsUntilNextNonSpace,
-  getCharsUntilNextSpace,
-  needsSpaceAfter,
+	getCharsUntilNextNonSpace,
+	getCharsUntilNextSpace,
+	needsSpaceAfter,
+	getAsciiArtInfo,
 } = require("./helpers");
+const { get } = require("https");
 
 let codeTokens;
 let asciiArt;
 
 async function main() {
-  const parser = new ArgumentParser({
-    description:
-      "converts javascript code to ascii art while mantaining its functionality",
-    add_help: true,
-  });
+	const parser = new ArgumentParser({
+		description:
+			"converts javascript code to ascii art while mantaining its functionality",
+		add_help: true,
+	});
 
-  parser.add_argument("in", { help: "input path" });
-  parser.add_argument("out", { help: "output path" });
-  parser.add_argument("ascii", { help: "path to file with ascii art" });
+	parser.add_argument("in", { help: "input path" });
+	parser.add_argument("out", { help: "output path" });
+	parser.add_argument("ascii", { help: "path to file with ascii art" });
 
-  const args = parser.parse_args();
+	const args = parser.parse_args();
 
-  let code = fs.readFileSync(args.in).toString();
+	let code = fs.readFileSync(args.in).toString();
 
-  // minify the code because i.e. long variable names make problems
-  // (they might not fit onto a single line of ascii art)
-  code = (await minify(code)).code;
+	// minify the code because i.e. long variable names make problems
+	// (they might not fit onto a single line of ascii art)
+	code = await minify(code, { sourceMap: true });
+	code = code.code;
 
-  codeTokens = tokenize(code);
+	codeTokens = tokenize(code);
 
-  asciiArt = fs.readFileSync(args.ascii).toString();
+	asciiArt = fs.readFileSync(args.ascii).toString();
 
-  insertToken(0);
+	insertToken(0);
 
-  fs.writeFile(args.out, outArray.join(""), () => {});
+	fs.writeFile(args.out, outArray.join(""), () => {});
 }
 
 let outArray = [];
 let asciiPos = 0;
 
 function insertToken(i) {
-  const t = codeTokens[i];
-  if (i >= codeTokens.length) {
-    return 0;
-  }
+	const t = codeTokens[i];
 
-  if (asciiArt[asciiPos] === "#") {
-    const charsUntilSpace = getCharsUntilNextSpace(asciiArt.slice(asciiPos));
-    if (charsUntilSpace > t.value.length) {
-      outArray.push(t.value);
-      asciiPos += t.value.length;
-      if (needsSpaceAfter(t, codeTokens[i + 1])) {
-        outArray.push(" ");
-        asciiPos++;
-      }
-      insertToken(i + 1);
-    } else if (charsUntilSpace >= 4) {
-      outArray.push(`/*${"#".repeat(charsUntilSpace - 4)}*/`);
-      asciiPos += charsUntilSpace;
-      insertToken(i);
-    } else {
-      outArray.push(" ".repeat(charsUntilSpace));
-      asciiPos += charsUntilSpace;
-      insertToken(i);
-    }
-  } else if (asciiArt[asciiPos] === " ") {
-    const charsUntilNonSpace = getCharsUntilNextNonSpace(
-      asciiArt.slice(asciiPos)
-    );
-    outArray.push(" ".repeat(charsUntilNonSpace));
-    asciiPos += charsUntilNonSpace;
-    insertToken(i);
-  } else if (asciiArt[asciiPos] === "\n") {
-    outArray.push("\n");
-    asciiPos++;
-    insertToken(i);
-  }
+	if (
+		i >= codeTokens.length &&
+		asciiPos < getAsciiArtInfo(asciiArt).totalNumberOfChars
+	) {
+		if (asciiArt[asciiPos] == "#") {
+			let charsUntilSpace = getCharsUntilNextSpace(asciiArt.slice(asciiPos));
+			if (charsUntilSpace >= 4) {
+				outArray.push(`/*${"#".repeat(charsUntilSpace - 4)}*/`);
+				asciiPos += charsUntilSpace;
+				insertToken(i);
+			}
+		} else if (asciiArt[asciiPos] == " ") {
+			let charsUntilNonSpace = getCharsUntilNextNonSpace(
+				asciiArt.slice(asciiPos)
+			);
+			outArray.push(" ".repeat(charsUntilNonSpace));
+			asciiPos += charsUntilNonSpace;
+			insertToken(i);
+		}
+	} else {
+		if (asciiArt[asciiPos] == "#") {
+			let charsUntilSpace = getCharsUntilNextSpace(asciiArt.slice(asciiPos));
+			if (charsUntilSpace > t.value.length) {
+				outArray.push(t.value);
+				asciiPos += t.value.length;
+				if (needsSpaceAfter(t, codeTokens[i + 1])) {
+					outArray.push(" ");
+					asciiPos++;
+				}
+				insertToken(i + 1);
+			} else if (charsUntilSpace >= 4) {
+				outArray.push(`/*${"#".repeat(charsUntilSpace - 4)}*/`);
+				asciiPos += charsUntilSpace;
+				insertToken(i);
+			} else {
+				outArray.push(" ".repeat(charsUntilSpace));
+				asciiPos += charsUntilSpace;
+				insertToken(i);
+			}
+		} else if (asciiArt[asciiPos] == " ") {
+			let charsUntilNonSpace = getCharsUntilNextNonSpace(
+				asciiArt.slice(asciiPos)
+			);
+			outArray.push(" ".repeat(charsUntilNonSpace));
+			asciiPos += charsUntilNonSpace;
+			insertToken(i);
+		} else if (asciiArt[asciiPos] == "\n") {
+			outArray.push("\n");
+			asciiPos++;
+			insertToken(i);
+		}
+	}
 }
 
 main();
